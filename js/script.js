2baec4edcf818e09b7b63ddf497e86d728035cce
Lydia <lydiadoula@gmail.com>
2023-04-23 20:42:59 +0100

    even more finish

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
       Javascript INDEX
 ============================
     1. Time countdown
-    2. Error/success message
+    2. Error/success message - "Notify Me" button
+    3. Error/success message - contact form 
+
 */
 
 (function($) {
@@ -15,7 +17,7 @@
     }, 2000);
 })
 
-//Time countdown
+// 1. Time countdown
 const seconds = document.querySelector(".seconds .number");
 minutes = document.querySelector(".minutes .number");
 hours = document.querySelector(".hours .number");
@@ -52,20 +54,23 @@ const timeFunction = setInterval(() => {
 
 }, 1000); // Speed of time
 
-// Error/success message after submitting e-mail for getting notified 
-function validate(){
+
+// 2. Error/success message after submitting e-mail for getting notified 
+function validateEmail(){
     "use strict";
     let email=document.getElementById("email").value;
     let error=document.getElementById("error1");
 
     if(!email) {
         error.textContent="Please enter your email address";
-        error.style.color = "red";
+        error.style.backgroundColor = "black";
+        error.style.color = "rgb(300, 0, 0)";
         return false;
     }
     else {
         let successMessage = "Thank you for subscribing!";
         error.textContent= successMessage;
+        error.style.backgroundColor = "black";
         error.style.color = "lightgreen";
         localStorage.setItem("successMessage", successMessage);
         return true;
@@ -78,15 +83,52 @@ function validate(){
         if (successMessage) {
             let error=document.getElementById("error1");
             error.textContent = successMessage;
+            error.style.backgroundColor = "black";
             error.style.color = "lightgreen";
             localStorage.removeItem("successMessage"); // Remove success message from localStorage
         }
     });
     
-    
-    
-    
-    
-    
+
+    // 3. Error/success message after submitting contact form 
+    function validateContactForm() {
+        "use strict";
+      
+        // Get the form elements
+        const name = document.getElementById("name1").value;
+        const email = document.getElementById("email1").value;
+        const message = document.getElementById("message").value;
+        const error = document.getElementById("error");
+      
+        // Validate the form fields
+          if (!message) {
+          error.textContent = "Please enter your message";
+          error.style.color = "red";
+          return false;
+        } else {
+          // Display success message
+          const successMessage = "Thank you for your message!";
+          error.textContent = successMessage;
+          error.style.color = "green";
+          localStorage.setItem("successMessage", successMessage);
+          return true;
+        }
+      }      
+      
+      window.addEventListener("load", function() {
+        "use strict";
+        let successMessage = localStorage.getItem("successMessage");
+        if (successMessage) {
+            let error=document.getElementById("error");
+            error.textContent = successMessage;
+            error.style.color = "green";
+            localStorage.removeItem("successMessage"); // Remove success message from localStorage
+        }
+    });
+      
+      
+      
+      
+      
 
 
